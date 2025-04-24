b4c29a166248ac8402ee99b18d905d5934260e0a
Thomas28 <thomasthanos28@gmail.com>
2025-04-24 04:41:03 +0300

    Update autoreadtrash.plugin.js

--- Discord_DEV/Plugins/autoreadtrash.plugin.js
+++ Discord_DEV/Plugins/autoreadtrash.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name AutoReadTrash
- * @version 5.5.4
+ * @version 5.5.5
  * @description Μαρκάρει φακέλους ως αναγνωσμένους με βάση τα ID τους, με το παλιό δεξί κλικ + click, responsive UI, Material-style settings και έλεγχο τιμών.
  * @author ThomasT
  * @authorId 706932839907852389
@@ -786,7 +786,8 @@ isNewerVersion(remote, local) {
 
 					this.showCustomToast("✅ Ενημερώθηκε! Γίνεται αυτόματη επανεκκίνηση...", "success");
 this._justUpdated = true;
-document.querySelectorAll(".layerContainer_da8173").forEach(el => el.remove());
+const modal = document.querySelector('[role="dialog"]');
+if (modal) modal.closest(".layerContainer_da8173")?.remove();
 setTimeout(() => BdApi.Plugins.reload("AutoReadTrash"), 1000);
 				} catch (err) {
 					console.error("Update failed:", err);
@@ -800,7 +801,7 @@ setTimeout(() => BdApi.Plugins.reload("AutoReadTrash"), 1000);
 
 
 	getVersion() {
-		return "5.5.4";
+		return "5.5.5";
 	}
 
 
